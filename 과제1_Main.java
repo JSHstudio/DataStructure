6130e9c0b24291a767cc14b6df041d61049e803a
JSHstudio <50391823+JSHstudio@users.noreply.github.com>
2019-09-27 22:18:45 +0900

    Update 과제1_Main.java

--- 과제1_Main.java
+++ 과제1_Main.java
@@ -1,27 +1,40 @@
-package Áø½ÃÈÆ_2018204006_°úÁ¦1;
+package JSH_2018204006_ê³¼ì œ1;
 
-public class °úÁ¦1_Main {
-
+public class ê´´ì œ1_Main {
+	/*
+	 * example of [ì‹¤ìŠµê³¼ì œ1]
+	 * 
+	 * @author JSH
+	 * @license GNU General Public License v2.0
+	 * @version 1.0
+	 * @see README
+	 * @see System.out.println
+	 * @Link https://github.com/JSHstudio/DataStructure
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] names = { "Rob", "Mike", "Rose", "Jill", "Jack", "Paul", "Bob" };
 		String[] removes = {"Mike", "Paul", "Bob"};
 		int[] scores = { 750, 1105, 590, 740, 610, 410, 840 };
 		int capacity = 5;
-
+		/*
+		* ArrayList Start
+		*/
 		Scoreboard board = new Scoreboard(capacity);
 		for (int i = 0; i < names.length; i++) {
 			GameEntry a = new GameEntry(names[i], scores[i]);
 			board.add(a);
 		}
-
 		try {
 			board.remove(3);
 			board.remove(1);
 		} catch (IndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
-
+		/*
+		* DoublyLinkedList Start;
+		*/
+		
 		DoublyLinkedList<GameEntry> doublyLinkedList = new DoublyLinkedList<GameEntry>();
 		for (int i = 0; i < names.length; i++) {
 			GameEntry b = new GameEntry(names[i], scores[i]);
@@ -32,12 +45,13 @@ public class °úÁ¦1_Main {
 			doublyLinkedList.remove(removes[i]);
 			System.out.println(doublyLinkedList.toString("removed"));
 		}
-		
-
+		/*
+		* Permutation Start();
+		*/
 		String str = "";
 		String set = "abc";
-		PuzzleSolve p = new PuzzleSolve();
-		p.puzzleSolve(3, str, set);
+		Permutation p = new Permutation();
+		p.permutation(3, str, set);
 	}
 
 }
